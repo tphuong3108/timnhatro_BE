6d648de73895dab91f303997b640da5daaf992d4
thwyya <ngothithuynga736@gmail.com>
2025-09-28 17:14:09 +0700

    gửi OTP và xác minh OTP

--- src/config/environment.js
+++ src/config/environment.js
@@ -8,4 +8,6 @@ export const env = {
   BUILD_MODE: process.env.BUILD_MODE,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+  EMAIL_USER: process.env.EMAIL_USER,
+  EMAIL_PASS: process.env.EMAIL_PASS,
 }
