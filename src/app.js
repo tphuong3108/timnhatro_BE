4ce25d2da001738ba8644b0643d5ae89f6352354
ChiThanh <ngchithanh777@gmail.com>
2025-12-19 21:27:51 +0700

    DeployEnd

--- src/app.js
+++ src/app.js
@@ -43,7 +43,9 @@ const START_SERVER = async () => {
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ extended: true, limit: "50mb" }));
   app.use(express.static(path.join(__dirname, "public")));
-
+  app.get("/", (req, res) => {
+  res.send("Hello! Welcome to TIMNHATRO Backend Server.");
+});
   // Routes
   app.use("/api/hosts", APIs);
   app.use("/api", APIs);
